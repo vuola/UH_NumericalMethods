45a2ccb636e5b894ddba160601dfbd785b0eb39a
vuola <markus.vuolahti@gmail.com>
2023-08-17 13:28:33 +0300

    test commit CMake

---
+++ exercise01/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv){
+
+    if (argc!=4) {
+        fprintf(stderr,"Usage: %s x y z\n",argv[0]);
+        return (1);
+    }
+
+    auto result = (10 <=> 20) > 0;
+    cout << argv[1] << endl;
+    cout << argv[2] << endl;
+    cout << argv[3] << endl;
+
+    if(result){
+        cout << "10 > 20\n";
+    }else{
+        cout << "10 < 20\n";
+    }
+}
