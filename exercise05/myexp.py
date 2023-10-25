42bdf3425fc95ac092d5a42b07eef9a04101c906
Markus Vuolahti <markus.vuolahti@gmail.com>
2023-10-25 07:41:18 +0300

    e5p2

--- exercise05/myexp.py
+++ exercise05/myexp.py
@@ -3,19 +3,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import randomgenerator as rg
 
-def exponential_approximation(x):
+def myexp(x):
     n = round(x / math.log(2))
     y = x - n * math.log(2)
-    result = math.exp(y) * 2**n
-    return y
+    return math.exp(y) * 2**n
 
-# Test with an example value of x
+# Test error distribution with example values of x
 
 x = rg.portable_generator(13456,79676,1000) * 20 - 10
 out = np.empty(1000)
 
 for i in range(1000):
-    out[i] = exponential_approximation(x[i])
+    out[i] = myexp(x[i]) - math.exp(x[i])
+    if out[i] < -1000:
+        print("x[i]: ", x[i])
 
 plt.hist(out, bins=80, density=True, label='normal', alpha=0.7, edgecolor='yellow')
 plt.show()
