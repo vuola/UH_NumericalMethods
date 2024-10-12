bbaffb7413a4e44ef8332c79cd668194eb6ce5ac
Markus Vuolahti <markus.vuolahti@gmail.com>
2024-10-12 21:38:04 +0300

    ex06all

--- exercise01/plotFunks.py
+++ exercise01/plotFunks.py
@@ -1,5 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+import sys
+import os
+
+# Get the absolute path to the build directory (relative to the script's location)
+current_dir = os.path.dirname(os.path.abspath(__file__))
+build_dir = os.path.join(current_dir, '..', 'build', 'exercise01')
+
+# Append the build directory to sys.path
+sys.path.append(build_dir)
+
+
 import funks as fu
 
 # Generate X values in a defined range and resolution
--- exercise03/plotJacobi.py
+++ exercise03/plotJacobi.py
@@ -1,5 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+import os
+
+# Get the absolute path to the build directory (relative to the script's location)
+current_dir = os.path.dirname(os.path.abspath(__file__))
+build_dir = os.path.join(current_dir, '..', 'build', 'exercise03')
+
+# Append the build directory to sys.path
+sys.path.append(build_dir)
+
 import jacobi as j
  
 N = np.array([8, 9, 10, 11, 12], np.int32) 
