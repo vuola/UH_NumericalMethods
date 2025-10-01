a86ffe47dce496fa53070fb03a41649cab60c96b
Markus Vuolahti <markus.vuolahti@gmail.com>
2025-10-01 07:37:25 +0300

    ex11p1

---
+++ exercise11/rungeKuttaSolver.cpp
@@ -0,0 +1,11 @@
+/**
+ * @brief Solve the equation y''(x)+y(x)=0 with initial conditions.
+ * @file rungeKuttaSolver.cpp
+ * @param a Initial value of x
+ * @param b Final value of x
+ * @param N Number of steps
+ * @param y0 Initial value of y at x=a
+ * @param yp0 Initial value of y' at x=a
+ * @param x Array to store x values (size N+1)
+ * @param y Array to store y values (size N+1)
+ */
